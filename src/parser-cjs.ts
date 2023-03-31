a25464156e1070fe56a409596dd69e0b1f049ade
happy wang <shuizhongyueming@gmail.com>
2023-03-31 16:04:00 +0800

    make parser more reusable

--- src/parser-cjs.ts
+++ src/parser-cjs.ts
@@ -6,54 +6,54 @@ import { visit, namedTypes } from 'ast-types';
 // https://github.com/dependents/node-detective-cjs
 // https://github.com/dependents/node-ast-module-types
 
+export const extractDependencyFromRequire = (node: namedTypes.CallExpression) => {
+  const arg = node.arguments[0];
+  if (namedTypes.Literal.check(arg) || namedTypes.StringLiteral.check(arg)) {
+    return arg.value as string;
+  }
+  if (namedTypes.TemplateLiteral.check(arg)) {
+    return arg.quasis[0].value.raw as string;
+  }
+};
+
+export const extractDependencyFromMainRequire = (node: namedTypes.CallExpression) => {
+  return (node.arguments[0] as any).value as string;
+};
+
+// ${requireFunctionNames}('xxx')
+const isPlainRequire = (node: namedTypes.CallExpression, requireFunctionNames: string[]) => {
+  return (
+    namedTypes.Identifier.check(node.callee) && requireFunctionNames.includes(node.callee.name)
+  );
+};
+
+// ${requireFunctionNames}.main.require('xxx')
+const isMainScopeRequire = (node: namedTypes.CallExpression, requireFunctionNames: string[]) => {
+  const { callee } = node;
+  return (
+    namedTypes.MemberExpression.check(callee) &&
+    namedTypes.MemberExpression.check(callee.object) &&
+    namedTypes.Identifier.check(callee.object.object) &&
+    requireFunctionNames.includes(callee.object.object.name) &&
+    namedTypes.Identifier.check(callee.object.property) &&
+    callee.object.property.name === 'main' &&
+    namedTypes.Identifier.check(callee.property) &&
+    callee.property.name === 'require'
+  );
+};
+
 export const parserCjs: (requireFunctionNames?: string[]) => ModuleParser = (
   requireFunctionNames = ['require']
 ) => {
-  // ${requireFunctionNames}('xxx')
-  const isPlainRequire = (node: namedTypes.CallExpression) => {
-    return (
-      namedTypes.Identifier.check(node.callee) && requireFunctionNames.includes(node.callee.name)
-    );
-  };
-
-  // ${requireFunctionNames}.main.require('xxx')
-  const isMainScopeRequire = (node: namedTypes.CallExpression) => {
-    const { callee } = node;
-    return (
-      namedTypes.MemberExpression.check(callee) &&
-      namedTypes.MemberExpression.check(callee.object) &&
-      namedTypes.Identifier.check(callee.object.object) &&
-      requireFunctionNames.includes(callee.object.object.name) &&
-      namedTypes.Identifier.check(callee.object.property) &&
-      callee.object.property.name === 'main' &&
-      namedTypes.Identifier.check(callee.property) &&
-      callee.property.name === 'require'
-    );
-  };
-
-  const extractDependencyFromRequire = (node: namedTypes.CallExpression) => {
-    const arg = node.arguments[0];
-    if (namedTypes.Literal.check(arg) || namedTypes.StringLiteral.check(arg)) {
-      return arg.value as string;
-    }
-    if (namedTypes.TemplateLiteral.check(arg)) {
-      return arg.quasis[0].value.raw as string;
-    }
-  };
-
-  const extractDependencyFromMainRequire = (node: namedTypes.CallExpression) => {
-    return (node.arguments[0] as any).value as string;
-  };
-
   return ({ code, moduleProcessor }) => {
     const ast = parse(code, { ecmaVersion: 'latest' });
     visit(ast, {
       visitCallExpression(nodePath) {
         const { node } = nodePath;
-        if (isPlainRequire(node)) {
+        if (isPlainRequire(node, requireFunctionNames)) {
           moduleProcessor(extractDependencyFromRequire(node));
           return false;
-        } else if (isMainScopeRequire(node)) {
+        } else if (isMainScopeRequire(node, requireFunctionNames)) {
           moduleProcessor(extractDependencyFromMainRequire(node));
           return false;
         }
